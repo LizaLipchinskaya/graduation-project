580cc9fc3129811f27d2ae9826d238fb4f6e51f1
LizaLipchinskaya <79473180+LizaLipchinskaya@users.noreply.github.com>
2022-09-25 23:42:41 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,8 @@
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-//        Controller controller = new Controller();
-//        Algorithm algorithm = controller.getAlgorithm();
-//        System.out.println(algorithm.findBannedProduct(1L));
-
-        Controller controller = new Controller();
-        Algorithm algorithm = controller.getAlgorithm();
-        List<String> newDish = algorithm.findChange(1L);
-
-        for (String dish : newDish) {
-            System.out.println(dish);
-        }
+        JdbcProvider provider = new JdbcProvider();
+        Algorithm algorithm = provider.getAlgorithm();
+        algorithm.doChanges(1L);
     }
 }
