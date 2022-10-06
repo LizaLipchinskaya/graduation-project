1b5820f962a2e4ffc59a273a1d6221f78d74a070
LizaLipchinskaya <79473180+LizaLipchinskaya@users.noreply.github.com>
2022-10-06 23:58:55 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        JdbcProvider provider = new JdbcProvider();
-        provider.getAlgorithm().doChanges(1L);
-        provider.getRightMenu().createNewMenu();
-    }
-}
